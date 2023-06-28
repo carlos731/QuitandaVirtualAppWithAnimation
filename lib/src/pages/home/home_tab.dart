922d00d8c43991ac04dd35e7d3a0ce7d347d0876
carlos731 <chtsca2001@gmail.com>
2023-06-28 12:31:42 -0300

    Adicionando animação de carregamento com shimmer

--- lib/src/pages/home/home_tab.dart
+++ lib/src/pages/home/home_tab.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import '../../config/custom_colors.dart';
 import '../common_widgets/app_name_widget.dart';
+import '../common_widgets/custom_shimmer.dart';
 import 'components/category_tile.dart';
 import 'package:app/src/config/app_data.dart' as appData;
 
@@ -12,7 +13,6 @@ import 'components/item_tile.dart';
 
 class HomeTab extends StatefulWidget {
   const HomeTab({super.key});
-  
 
   @override
   State<HomeTab> createState() => _HomeTabState();
@@ -34,6 +34,20 @@ class _HomeTabState extends State<HomeTab> with AutomaticKeepAliveClientMixin {
   }
 
   final UtilsServices utilsServices = UtilsServices();
+
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+  
+    Future.delayed(const Duration(seconds: 2), 
+    (){
+      setState(() {
+        isLoading = false;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -137,7 +151,7 @@ class _HomeTabState extends State<HomeTab> with AutomaticKeepAliveClientMixin {
                 right: 25,
               ),
               height: 40,
-              child: ListView.separated(
+              child: !isLoading ? ListView.separated(
                 scrollDirection: Axis.horizontal,
                 itemBuilder: (_, index) {
                   return CategoryTile(
@@ -152,28 +166,54 @@ class _HomeTabState extends State<HomeTab> with AutomaticKeepAliveClientMixin {
                 },
                 separatorBuilder: (_, index) => const SizedBox(width: 10),
                 itemCount: appData.categories.length,
+              ) : ListView(
+                scrollDirection: Axis.horizontal,
+                children: List.generate(10, (index) => Container(
+                    alignment: Alignment.center,
+                    margin: const EdgeInsets.only(right: 12),
+                    child: CustomShimmer(
+                      height: 20,
+                      width: 60,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),)
               ),
             ),
 
             // Grid
             Expanded(
-              child: GridView.builder(
-                padding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
-                physics: const BouncingScrollPhysics(),
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  mainAxisSpacing: 10,
-                  crossAxisSpacing: 10,
-                  childAspectRatio: 9 / 11.5,
-                ),
-                itemCount: appData.items.length,
-                itemBuilder: (_, index) {
-                  return ItemTile(
-                    item: appData.items[index],
-                    cartAnimationMethod: itemSelectedCartAnimations,
-                  );
-                },
-              ),
+              child: !isLoading
+                  ? GridView.builder(
+                      padding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
+                      physics: const BouncingScrollPhysics(),
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        mainAxisSpacing: 10,
+                        crossAxisSpacing: 10,
+                        childAspectRatio: 9 / 11.5,
+                      ),
+                      itemCount: appData.items.length,
+                      itemBuilder: (_, index) {
+                        return ItemTile(
+                          item: appData.items[index],
+                          cartAnimationMethod: itemSelectedCartAnimations,
+                        );
+                      },
+                    )
+                  : GridView.count(
+                      padding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
+                      physics: const BouncingScrollPhysics(),
+                      crossAxisCount: 2,
+                      mainAxisSpacing: 10,
+                      crossAxisSpacing: 10,
+                      childAspectRatio: 9 / 11.5,
+                      children: List.generate(10, (index) => CustomShimmer(
+                          height: double.infinity,
+                          width: double.infinity,
+                          borderRadius: BorderRadius.circular(20),
+                        ),),
+                    ),
             ),
           ],
         ),
@@ -181,5 +221,3 @@ class _HomeTabState extends State<HomeTab> with AutomaticKeepAliveClientMixin {
     );
   }
 }
-
-
